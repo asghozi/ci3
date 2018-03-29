2d95bfcef34b309b7d3de8b0bfa712bcaa2b5453
GHOZI\axioo <saifullahghozi@gmail.com>
2018-03-29 10:45:04 +0700

    Tugas01_1

---
+++ application/views/about.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello! </title>
+	<link rel="stylesheet" media="all"  type="text/css" href="././assets/css/bootstrap.min.css">
+<link rel="stylesheet" media="all"  type="text/css" href="././assets/css/bootstrap.css">
+<link rel="stylesheet" media="all"  type="text/css" href="././assets/css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="././assets/css/bootstrap.css">
+<script type="text/javascript" src="././assets/js/jquery.js"></script>
+<script type="text/javascript" src="././assets/js/bootstrap.js"></script>
+</head>
+</nav>
+<div class="btn-default"><h1><b>ABOUT</b></h1></div>
+</body>
+</html>
